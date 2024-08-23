42c162a95d9d04905884b7c3d2bf99691300109a
IllyaGY <igordstudy12@gmail.com>
2024-08-23 01:15:26 -0700

    Refactored

--- Chess/Game.cpp
+++ Chess/Game.cpp
@@ -146,37 +146,30 @@ void Game::undo(sf::RenderWindow *window, Field *field) {
 }
 
 
+
+
 void Game::check(Field* field) {													//CHECK IF KNIGHT DOES IT TOO!!!!!!!!	
-	for (auto king : kings) {
+	for (auto king : kings) {												//After the updateKingThread function, we can have a vector of all the enemy figures that posses a check on our king
 		auto figure = getType<King>(field->figurePoint(king));
-		for (auto i : figure->getChecks()) {
-			if (figure->diagPosEnemy(i, figure->getPos())) {
-				if ((getType<Queen>(field->figurePoint(i)) || getType<Bishop>(field->figurePoint(i)))) {
-					if (checkS != -1 && checkS != currSideMove) {
-						checkText.setString("Check Mate");
-						endGame = true; 
+		for (auto i : figure->getChecks()) {								//Get all the figures, making up check condition on our king
+			if (figure->diagPosEnemy(i, figure->getPos())) {				//If any of the diagonal direction are open to our king, it checks till it meets an enemy figures
+				if ((getType<Queen>(field->figurePoint(i)) ||				//Each Queen, Bishop and Pawn, can only attack it diagonally, though a Pawn can only attack if it is one square apart
+					getType<Bishop>(field->figurePoint(i))) ||				//thats where the chickHit comes in handy
+					getType<Pawn>(field->figurePoint(i))->checkHit(i, king)) {
+					if (checkS != -1 && checkS != currSideMove) {			//If checkS at this point was already not -1 meaning check was already placed on the king, and checkS doesnt equal the current side moving
+						checkText.setString("Check Mate");					//Mate is placed
+						endGame = true;										//Ending the game 
 						return;
 					}
-					else {
+					else {													//If no check was yet, we place a check for that king
 						checkS = field->figurePoint(i)->getSide();
 						return;
 					}
 				}
-				if (auto figCheck = getType<Pawn>(field->figurePoint(i))) {
-					if (figCheck->checkHit(i, king)) {
-						if (checkS != -1 && checkS != currSideMove) {
-							checkText.setString("Check Mate");
-							endGame = true; 
-							return;
-						}
-						else {
-							checkS = field->figurePoint(i)->getSide();
-							return;
-						}
-					}
-				}
 			}
-			else if (figure->XYPosEnemy(i, figure->getPos()) && (getType<Queen>(field->figurePoint(i)) || getType<Rook>(field->figurePoint(i)))) {
+			else if (figure->XYPosEnemy(i, figure->getPos()) &&				//In the XY plane, Queen, Rook are the ones dangerous
+				(getType<Queen>(field->figurePoint(i)) ||
+				getType<Rook>(field->figurePoint(i)))) {
 				if (checkS != -1)checkText.setString("Check Mate");
 				checkS = field->figurePoint(i)->getSide();		
 				return;
@@ -245,7 +238,7 @@ void Game::different(std::shared_ptr<Figure> piece, Field*field) {
 	auto pawn = getType<Pawn>(piece);				//The position of the pawn is new at this point
 	if (pawn){
 		if (pawn->checkIfEnPass()) {
-			currentEnPassPawn = (pawn);					//If an by en Pass is applicable to this pawn, set a pointer to it
+			currentEnPassPawn = (pawn);					//If an attack by en Pass is applicable to this pawn, set a pointer to it
 			helpDiff(pawn, LEFT_BORDER, field, -1);
 			helpDiff(pawn, RIGHT_BORDER, field, 1);
 
@@ -262,9 +255,9 @@ void Game::different(std::shared_ptr<Figure> piece, Field*field) {
 
 
 void Game::move(int moveTo, Field *field, int lastSel, int fieldPos) {
-	auto piece = (lastSel != -1) ? playerBase.at(lastSel) : field->figurePoint(fieldPos);
-	piece->setPos(field->getCoord(moveTo).x, field->getCoord(moveTo).y);																	//set the pos of the last selected figure to the pos of the cube clicked
-	this->addToSide(currSideMove, field->emplaceBoard(piece->getPos(), moveTo));															//emplace the old figure, if any, with the new one																			
+	auto piece = (lastSel != -1) ? playerBase.at(lastSel) : field->figurePoint(fieldPos);		//If lastSel index is provided, access the pointer straight from the playerBase vector
+	piece->setPos(field->getCoord(moveTo).x, field->getCoord(moveTo).y);						//set the pos of the last selected figure to the pos of the cube clicked
+	this->addToSide(currSideMove, field->emplaceBoard(piece->getPos(), moveTo));				//emplace the old figure, if any, with the new one																			
 	piece->setCubePos(moveTo);			
 	different(piece, field);	
 	piece->setFirstFalse();
@@ -275,79 +268,65 @@ void Game::move(int moveTo, Field *field, int lastSel, int fieldPos) {
 
 
 
+void rookCastlingHelper(int king, int rook, std::shared_ptr<King> kingFigure) {
+	if (king) {
+		if (rook % 2 == 0) kingFigure->setCastlingRight(false);
+		else kingFigure->setCastlingLeft(false);
+	}
+	else
+		if (rook % 2 == 0) kingFigure->setCastlingLeft(false);
+		else kingFigure->setCastlingRight(false);
+	
+}
+
+
 void Game::updateKingsThreads(Field* field, int lastSel, int input, int* KingsPos) {
 
 
 	for (int i = 0; i < 2; i++) {
-		playerBase.at(KingsPos[i])->updateMoves(field);			//Update both kings
+		playerBase.at(KingsPos[i])->updateMoves(field);			//Update both kings and their threads
 	}
 
-	if (std::shared_ptr<King> figure = std::dynamic_pointer_cast<King>(playerBase.at(lastSel))) {
+	if (std::shared_ptr<King> figure = std::dynamic_pointer_cast<King>(playerBase.at(lastSel))) {	//If one of the kings was chosen to move, update global king position
 		kings[figure->getSide()] = input;
-		figure->setFirstFalse();
-		figure->setCastling();
-		if (field->isCastleSquare(figure->getPos())) {
-			std::vector<int> ptr = figure->getRook(figure->getPos());
-
+		figure->setFirstFalse();	
+		figure->setCastling();													//Castling set to false
+		if (field->isCastleSquare(figure->getPos())) {							//If the new position of the king belongs to castling positions, the corresponding rook is moved
+			std::vector<int> ptr = figure->getRook(figure->getPos());		
 			move(ptr.at(1), field, -1, ptr.at(0));
 			
 		}
 	}
-
-	
-
-	
-
-	for (int i = 0; i < 2; i++) {
-		if (getType<King>(KingsPos[i])->getFirst()) {														//Fix ERROR 
-			for (int j = i * 2; j < (i + 1) * 2; j++) {
-				if (auto figure = getType<Rook>(field->figurePoint(rookPos[j]))) {						//If the rook is in the starting position
-					if (!figure->isFirst())																//Check if it didn't make any moves yet, because it could simply go back to the starting pos
-						if (i) {
-							if (j % 2 == 0) getType<King>(field->figurePoint(kings[i]))->setCastlingRight(false);
-							else getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-						}
-						else
-							if (j % 2 == 0)getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-							else getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-				}
-				else {
-					if (i) {
-						if (j % 2 == 0) getType<King>(field->figurePoint(kings[i]))->setCastlingRight(false);						//REFACTOR
-						else getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-					}
-					else
-						if (j % 2 == 0)getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-						else getType<King>(field->figurePoint(kings[i]))->setCastlingLeft(false);
-				}
-
+	for (int king = 0; king < 2 && getType<King>(KingsPos[king])->getFirst(); king++) {				//Among the two Kings if it still hasn't done any moves yet 									 
+		for (int rook = king * 2; rook < (king + 1) * 2; rook++) {									//0 and 1 are for the White King - index 0, and 2 and 3 are for the Black King - index 1
+			if (auto figure = getType<Rook>(field->figurePoint(rookPos[rook]))) {						//If the rook is in the starting position
+				if (!figure->isFirst())																//Check if it didn't make any moves yet, because it could simply go back to the starting pos
+					rookCastlingHelper(king, rook, getType<King>(field->figurePoint(kings[king])));
+				else 
+					rookCastlingHelper(king, rook, getType<King>(field->figurePoint(kings[king])));
 			}
 
 		}
 	}
-
-
-
-	delete KingsPos;
-
+	delete[] KingsPos;
 }
+
+
+
+
+
+
 
 
 void Game::makeMove(sf::RenderWindow* window, Field* field) {
 		sf::Vector2f clickPos = window->mapPixelToCoords(sf::Mouse::getPosition(*window));								
 		int input = field->cubesClicked(clickPos, this);																
 		if (input > -1) {																								//if clicked on a cube that is currently active
-			
-
-			move(input, field, lastSel);																			//set the pos of the figure to input - cube inde	
-																																
-			updateKingsThreads(field, lastSel, input, new int[2] { KING_LIST_WHITE, KING_LIST_BLACK } );
-			
-			
-			check(field);
-
-			currSideMove = currSideMove > 0 ? 0 : 1;
-			switchMarkerPos(currSideMove);
+			move(input, field, lastSel);																			//Move the object																													
+			updateKingsThreads(field, lastSel, input, new int[2] { KING_LIST_WHITE, KING_LIST_BLACK } );			//Update the current kings check mate condition. 
+			check(field);																							//Check condition
+			currSideMove = currSideMove > 0 ? 0 : 1;																//Switch current playing side
+			switchMarkerPos(currSideMove);																			//Switch the green marker
 
 			
 			
